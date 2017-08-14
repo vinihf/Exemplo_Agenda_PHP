0686d83ab4b3b59f82d703d08f27b3ef0c845b4f
Vinicius Hartmann Ferreira <vinihf@gmail.com>
2017-08-14 20:55:46 -0300

    Formatação com Bootstrap 4

--- visao/cadContato.php
+++ visao/cadContato.php
@@ -7,17 +7,35 @@ if(isset($_POST['botao']) && $_POST['botao']=="Adicionar"){
 ?>
 <html lang='pt-br'>
 <head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
 <meta charset='utf-8'>
 <title>Agenda de Contatos</title>
 </head>
 <body>
+<div class='container-fluid'>
+<div class="row justify-content-center">
+<div class="col-5">
 <form method='post' action='cadContato.php'>
-	Nome: <input type='text' name='nome'>
-	<br>
-	Numero: <input type='tel' name='numero'>
-	<br>
-	<input type='submit' name='botao' value='Adicionar'>
+<div class="form-group row">
+    <label for="nome" class="col-sm-2 col-form-label">Nome:</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="nome" name='nome' required>
+    </div>
+</div>
+<div class="form-group row">
+    <label for="numero" class="col-sm-2 col-form-label">Número:</label>
+    <div class="col-sm-10">
+      <input type="tel" class="form-control" id="numero" name='numero' required>
+    </div>
+ </div>
+<input type='submit' name='botao' value='Adicionar'>
 </form>
 <a href='../index.html'>Voltar</a>
+</div>
+</div>
+</div>
 </body>
 </html>
