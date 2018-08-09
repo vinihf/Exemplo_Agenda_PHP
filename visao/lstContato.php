4aede1fb44aef89cf9f36dc828ba645268ce0d78
marcelorambomartins <30699523+marcelorambomartins@users.noreply.github.com>
2018-08-09 01:00:27 -0600

    CSS de Listagem de Contatos

--- visao/lstContato.php
+++ visao/lstContato.php
@@ -10,19 +10,31 @@ $contatos = $cContato->listarTodos();
 ?>
 <html lang='pt-br'>
 <head>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.css" type='text/css'>
+
 <meta charset='utf-8'>
 <title>Agenda de Contatos</title>
 </head>
 <body>
 <?php
 	if($contatos!=false){
+
+			echo "<table class='table'><thead><tr><th>Nome</th><th>Número</th><th>Ações</th></thead><tbody>";
+
 		foreach($contatos as $contato){
-			echo $contato->getNome()." - ".$contato->getNumero();
+			echo "<tr><td>";
+			echo $contato->getNome()." </td><td> ".$contato->getNumero();
+			echo "</td><td>";
 			echo "<a href='lstContato.php?id=".$contato->getId()."'>Excluir</a>";
-			echo " | ";
-			echo "<a href='editContato.php?id=".$contato->getId()."'>Editar</a>";
-			echo "<br>";
+			echo "<a href='editContato.php?id=".$contato->getId()."'>       |      Editar</a>";
+			echo "</td></tr>";
 		}
+
+		   echo "</tbody><table>";
 	}
 ?>
 <br>
